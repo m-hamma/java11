8c9f9fa54e83a9c41182712598fdc7ed4c2bb3fc
mhamma <mhamma@gmail.com>
2020-06-25 16:51:20 +0200

    premier commit

---
+++ daomodule/src/main/java/com/hamma/Dao.java
@@ -0,0 +1,8 @@
+package com.hamma;
+public interface Dao<T> {
+ 
+    Optional<T> findById(int id);
+ 
+    List<T> findAll();
+ 
+}
---
+++ entitymodule/src/main/java/com/hamma/User.java
@@ -0,0 +1,9 @@
+package com.hamma;
+@Data
+public class User {
+ 
+    private final String name;
+ 
+    // standard constructor / getter / toString
+ 
+}
