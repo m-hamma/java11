19f3e3ddda768ce6c3c50e4c1c70a5e7279ed826
hamma <mohamedhamma445@gmail.com>
2021-03-24 23:58:50 +0100

    ajout mapper et junit

--- userdaomodule/src/main/java/com/hamma/mapper/UserMapper.java
+++ userdaomodule/src/main/java/com/hamma/mapper/UserMapper.java
@@ -2,6 +2,7 @@ package com.hamma.mapper;
 
 import com.hamma.dtos.UserDto;
 import com.hamma.entity.User;
+@Mapper
 public interface UserMapper {
 	/*@Mapping(source = "name", target = "name")
     @Mapping(source = "firstName", target = "firstName")
